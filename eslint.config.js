dc3530080e435421e4e38827b02c8ce69204ff7e
Kevin L Caswick <24442686+kcaswick@users.noreply.github.com>
2023-08-30 12:47:13 -0500

    Add eslint

---
+++ eslint.config.js
@@ -0,0 +1,46 @@
+import deprecation from "eslint-plugin-deprecation";
+import { FlatCompat } from "@eslint/eslintrc";
+import js from "@eslint/js";
+import prettier from "eslint-plugin-prettier";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
+import ts from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
+import xo from "eslint-config-xo";
+// @ts-check
+
+const compat = new FlatCompat();
+
+/**
+ * @type {import('eslint').Linter.Config}
+ */
+export default [
+  js.configs.recommended,
+  ...compat.config(ts.configs["eslint-recommended"]),
+  ...compat.extends("plugin:@typescript-eslint/recommended"),
+  ...compat.config(xo),
+  ...compat.config(prettier.configs.recommended),
+  {
+    languageOptions: {
+      globals: {
+        node: true,
+      },
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: 2022,
+        project: "./tsconfig.json",
+        sourceType: "module",
+        warnOnUnsupportedTypeScriptVersion: false,
+      },
+    },
+    plugins: { deprecation, ts, simpleImportSort },
+    linterOptions: {
+      reportUnusedDisableDirectives: true,
+    },
+    rules: {
+      "prettier/prettier": "warn",
+      "no-redeclare": "off",
+      "@typescript-eslint/no-redeclare": ["error"],
+      "no-unused-vars": "off",
+    },
+  },
+];

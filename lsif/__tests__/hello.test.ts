33ba9b98b38a74462bd2a4004018405ebb026bcb
Kevin C <lablua@gmail.com>
2024-11-08 18:55:15 -0600

    Setup vitest
    - Make sure it uses vite version compatible with node 16

---
+++ lsif/__tests__/hello.test.ts
@@ -0,0 +1,7 @@
+import { describe, it, expect } from 'vitest';
+
+describe('Hello World Test', () => {
+    it('should return true for 1 + 1 equals 2', () => {
+        expect(1 + 1).toBe(2);
+    });
+});
